4f47f4b80fe06f1a91198311b86620793df67ccb
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-04-02 11:55:41 -0600

    include fs, move to async tests

--- src/accountListParse.js
+++ src/accountListParse.js
@@ -1,6 +1,14 @@
+const { readFile } = require("fs.promises")
+// import { readFile } from "fs/promises";
 
-const accountListParse = () => {
-  return null
+const accountListParse = async (filePath) => {
+  if (!filePath) {
+    return null
+  }
+
+  const accountList = await readFile(filePath, {encoding: "utf8"})
+
+  return accountList
 }
 
 module.exports = accountListParse
--- test/accountListParse.test.js
+++ test/accountListParse.test.js
@@ -1,7 +1,29 @@
 const accountListParse = require("../src/accountListParse")
 
-describe("when given no parameters", () => {
-  it("returns null", () => {
-    expect(accountListParse()).toBe(null)
+describe("when given no filepath", () => {
+  test("it returns null", () => {
+    return accountListParse().then( data => {
+      expect(data).toBe(null)
+    } )
+  })
+})
+
+describe("when given a filepath", () => {
+  const expectedAccountNumbers =
+    `000000000
+    111111111
+    222222222
+    333333333
+    444444444
+    555555555
+    666666666
+    777777777
+    888888888
+    999999999
+    123456789`
+  test("it returns valid accounts", () => {
+    return(accountListParse("test/fixtures/sampleAccountList.txt")).then( data => {
+      expect(data).toBe(expectedAccountNumbers)
+    })
   })
 })
