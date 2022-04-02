20fcfe35f28146ba892a05d2731d40b8725301c7
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-04-02 11:22:38 -0600

    tidy specs

--- test/accountNumberParse.test.js
+++ test/accountNumberParse.test.js
@@ -1,32 +1,5 @@
 const accountNumberParse = require("../src/accountNumberParse")
 
-
-const zeroFixture =
-  ` _  _  _  _  _  _  _  _  _ 
-| || || || || || || || || |
-|_||_||_||_||_||_||_||_||_|
-
-`
-// => 000000000`
-
-
-const oneFixture =
-  `                           
-  |  |  |  |  |  |  |  |  |
-  |  |  |  |  |  |  |  |  |
-  
-`
-
-const allNumsFixture =
-  `    _  _     _  _  _  _  _ 
-  | _| _||_||_ |_   ||_||_|
-  ||_  _|  | _||_|  ||_| _|
-`
-// => 123456789`
-
-// => 111111111`
-
-
 describe("when given no input", () => {
   test("it returns null", () => {
     expect(accountNumberParse()).toBe(null)
@@ -34,19 +7,36 @@ describe("when given no input", () => {
 })
 
 describe("when given all zeros", () => {
+  const numberSet =
+    ` _  _  _  _  _  _  _  _  _ 
+| || || || || || || || || |
+|_||_||_||_||_||_||_||_||_|
+
+`
   test("it returns account number 000000000", () => {
-    expect(accountNumberParse(zeroFixture)).toBe("000000000")
+    expect(accountNumberParse(numberSet)).toBe("000000000")
   })
 })
 
 describe("when given all ones", () => {
+  const numberSet =
+    `                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |
+  
+`
   test("it returns account number 111111111", () => {
-    expect(accountNumberParse(oneFixture)).toBe("111111111")
+    expect(accountNumberParse(numberSet)).toBe("111111111")
   })
 })
 
 describe("when given all numbers", () => {
+  const numberSet =
+    `    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+`
   test("it returns account number 123456789", () => {
-    expect(accountNumberParse(allNumsFixture)).toBe("123456789")
+    expect(accountNumberParse(numberSet)).toBe("123456789")
   })
 })
