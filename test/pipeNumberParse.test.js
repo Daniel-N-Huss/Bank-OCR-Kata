20f137812cd0b30425779ef19afd7cb2769b56df
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-04-02 11:27:14 -0600

    cleanup specs

--- test/pipeNumberParse.test.js
+++ test/pipeNumberParse.test.js
@@ -1,16 +1,5 @@
 const pipeNumberParse = require("../src/pipeNumberParse")
 
-const zeroInput = [' _ ', '| |', '|_|']
-const oneInput = ['   ', '  |', '  |']
-const twoInput = [' _ ', ' _|', '|_ ']
-const threeInput = [' _ ', ' _|', ' _|']
-const fourInput = ['   ', '|_|', '  |']
-const fiveInput = [' _ ', '|_ ', ' _|']
-const sixInput = [' _ ', '|_ ', '|_|']
-const sevenInput = [' _ ', '  |', '  |']
-const eightInput = [' _ ', '|_|', '|_|']
-const nineInput = [' _ ', '|_|', ' _|']
-
 describe("when given no input", () => {
   test("it returns null", () => {
     expect(pipeNumberParse()).toBe(null)
@@ -18,61 +7,71 @@ describe("when given no input", () => {
 })
 
 describe("when given a pattern that matches zero", () => {
+  const pipePatterns = [' _ ', '| |', '|_|']
   test("it returns string '0'", () =>{
-    expect(pipeNumberParse(zeroInput)).toBe('0')
+    expect(pipeNumberParse(pipePatterns)).toBe('0')
   })
 })
 
 describe("when given a pattern that matches one", () => {
+  const pipePatterns = ['   ', '  |', '  |']
   test("it returns string '1'", () =>{
-    expect(pipeNumberParse(oneInput)).toBe('1')
+    expect(pipeNumberParse(pipePatterns)).toBe('1')
   })
 })
 
 describe("when given a pattern that matches two", () => {
+  const pipePatterns = [' _ ', ' _|', '|_ ']
   test("it returns string '2'", () =>{
-    expect(pipeNumberParse(twoInput)).toBe('2')
+    expect(pipeNumberParse(pipePatterns)).toBe('2')
   })
 })
 
 describe("when given a pattern that matches three", () => {
+  const pipePatterns = [' _ ', ' _|', ' _|']
   test("it returns string '3'", () =>{
-    expect(pipeNumberParse(threeInput)).toBe('3')
+    expect(pipeNumberParse(pipePatterns)).toBe('3')
   })
 })
 
 describe("when given a pattern that matches four", () => {
+  const pipePatterns = ['   ', '|_|', '  |']
   test("it returns string '4'", () =>{
-    expect(pipeNumberParse(fourInput)).toBe('4')
+    expect(pipeNumberParse(pipePatterns)).toBe('4')
   })
 })
 
 describe("when given a pattern that matches five", () => {
+  const pipePatterns = [' _ ', '|_ ', ' _|']
   test("it returns string '5'", () =>{
-    expect(pipeNumberParse(fiveInput)).toBe('5')
+    expect(pipeNumberParse(pipePatterns)).toBe('5')
   })
 })
 
 describe("when given a pattern that matches six", () => {
+  const pipePatterns = [' _ ', '|_ ', '|_|']
   test("it returns string '6'", () =>{
-    expect(pipeNumberParse(sixInput)).toBe('6')
+    expect(pipeNumberParse(pipePatterns)).toBe('6')
   })
 })
 
 describe("when given a pattern that matches seven", () => {
+  const pipePatterns = [' _ ', '  |', '  |']
   test("it returns string '7'", () =>{
-    expect(pipeNumberParse(sevenInput)).toBe('7')
+    expect(pipeNumberParse(pipePatterns)).toBe('7')
   })
 })
 
 describe("when given a pattern that matches eight", () => {
+  const pipePatterns = [' _ ', '|_|', '|_|']
   test("it returns string '8'", () =>{
-    expect(pipeNumberParse(eightInput)).toBe('8')
+    expect(pipeNumberParse(pipePatterns)).toBe('8')
   })
 })
 
 describe("when given a pattern that matches nine", () => {
+  const pipePatterns = [' _ ', '|_|', ' _|']
   test("it returns string '9'", () =>{
-    expect(pipeNumberParse(nineInput)).toBe('9')
+    expect(pipeNumberParse(pipePatterns)).toBe('9')
   })
 })
