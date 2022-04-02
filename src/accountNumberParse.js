9da1fd4884f5a1ccd101e8cd245e3fc1c2dc634e
Daniel Huss <63384617+Daniel-N-Huss@users.noreply.github.com>
2022-04-02 11:18:28 -0600

    handle all 1s case

--- src/accountNumberParse.js
+++ src/accountNumberParse.js
@@ -1,10 +1,26 @@
+const pipeNumberParse = require("./pipeNumberParse")
+
 
 const accountNumberParse = (accountNumberAsPipes) => {
   if (!accountNumberAsPipes) {
     return null
   }
 
-  return "000000000"
+  const numberRows = accountNumberAsPipes.split("\n")
+
+  const topRow = numberRows[0].match(/.{1,3}/g) //regex to place into 3 character groups
+  const middleRow = numberRows[1].match(/.{1,3}/g)
+  const bottomRow = numberRows[2].match(/.{1,3}/g)
+
+  let result = ""
+
+  for (let i = 0; i < 9; i++) {
+    const numberPipeSet = [topRow[i], middleRow[i], bottomRow[i]]
+
+    result += pipeNumberParse(numberPipeSet)
+  }
+
+  return result
 }
 
 module.exports = accountNumberParse
--- test/accountNumberParse.test.js
+++ test/accountNumberParse.test.js
@@ -10,6 +10,16 @@ const zeroFixture =
 // => 000000000`
 
 
+const oneFixture =
+  `                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |
+  
+`
+
+// => 111111111`
+
+
 describe("when given no input", () => {
   test("it returns null", () => {
     expect(accountNumberParse()).toBe(null)
@@ -19,5 +29,11 @@ describe("when given no input", () => {
 describe("when given all zeros", () => {
   test("it returns account number 000000000", () => {
     expect(accountNumberParse(zeroFixture)).toBe("000000000")
+  })
+})
+
+describe("when given all ones", () => {
+  test("it returns account number 111111111", () => {
+    expect(accountNumberParse(oneFixture)).toBe("111111111")
   })
 })
